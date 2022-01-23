8205ec78a8bd8957216aa2d9105bf55d18a9fdce
zxc5356061 <zxc5356061@gmail.com>
2022-01-24 00:52:35 +0800

    Update EditProfileEntity

---
+++ ClothesToU.Site/Models/Core/Interfaces/IEditMemberDataRepository.cs
@@ -0,0 +1,19 @@
+﻿using ClothesToU.Site.Models.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClothesToU.Site.Models.Core.Interfaces
+{
+    internal interface IEditMemberDataRepository
+    {
+
+		MemberEntity Load(string account);
+		void Update(EditProfileEntity entity);
+
+		MemberEntity Load(int memberId);
+		void UpdatePassword(int memberId, string encryptedPassword);
+	}
+}
--- ClothesToU.Site/Models/Entities/EditProfileEntity.cs
+++ ClothesToU.Site/Models/Entities/EditProfileEntity.cs
@@ -8,10 +8,11 @@ namespace ClothesToU.Site.Models.Entities
 {
     public class EditProfileEntity
     {
+        public int Id { get; set; }
         public string Name { get; set; }
-
+        public bool IsConfirmed { get; set; }
+        public string ConfirmCode { get; set; }
         public string Mobile { get; set; }
-
         public string Address { get; set; }
 
     }

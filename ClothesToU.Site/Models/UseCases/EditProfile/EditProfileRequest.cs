bef571f86634ea22383989a1c3a90b9598309f31
zxc5356061 <zxc5356061@gmail.com>
2022-01-25 13:52:10 +0800

    public class EditProfileRequest  {   public string Account { get; set; }   public string Name { get; set; }   public string Mobile { get; set; }   public string Address { get; set; }  }

--- ClothesToU.Site/Models/UseCases/EditProfile/EditProfileRequest.cs
+++ ClothesToU.Site/Models/UseCases/EditProfile/EditProfileRequest.cs
@@ -7,11 +7,8 @@ namespace ClothesToU.Site.Models.UseCases.EditProfile
 {
     public class EditProfileRequest
 	{
-		public int Id { get; set; }
 		public string Account { get; set; }
 		public string Name { get; set; }
-		public bool IsConfirmed { get; set; }
-		public string ConfirmCode { get; set; }
 		public string Mobile { get; set; }
 		public string Address { get; set; }
 	}

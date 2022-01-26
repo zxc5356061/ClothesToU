e56f34b7fcf9b14f798f63efa60efe53fcbd0e76
zxc5356061 <zxc5356061@gmail.com>
2022-01-26 15:28:19 +0800

    [v] add /Models/Entities/CartItemEntity   -public CartItemEntity(CartProductEntity product, int qty)    {     Product = product;     Qty = qty;    }   -public int Id { get; set; }   -public CartProductEntity Product{ get; set; }   -public int Qty{ get; set; }   -public int SubTotal{get { return Product.Price * Qty; }}

--- ClothesToU.Site/Models/Entities/Cart/CartItemEntity.cs
+++ ClothesToU.Site/Models/Entities/Cart/CartItemEntity.cs
@@ -7,28 +7,44 @@ namespace ClothesToU.Site.Models.Entities.Cart
 {
     public class CartItemEntity
     {
+		//Constructor
 		public CartItemEntity(CartProductEntity product, int qty)
 		{
 			Product = product;
 			Qty = qty;
-		}
+		}//Constructor_End
 
 		public int Id { get; set; }
+
 
 		private CartProductEntity _Product;
 		public CartProductEntity Product
 		{
-			get => _Product;
-			set => _Product = value != null ? value : throw new Exception("Product不能是null");
+            //get => _Product;
+            //set => _Product = value != null ? value : throw new Exception("Product不能是null");
+            get
+            {   return this._Product;}
+			set
+            {
+				this._Product = value != null ? value : throw new Exception("Product不能是Null");
+            }
 		}
 
 		private int _Qty;
 		public int Qty
 		{
-			get => _Qty;
-			set => _Qty = value > 0 ? value : throw new Exception("Qty必須是正數");
+			//get => _Qty;
+			//set => _Qty = value > 0 ? value : throw new Exception("Qty必須是正數");
+			get { return this._Qty; }
+            set
+            {
+				this._Qty = value > 0 ? value : throw new Exception("Qty必須是正數");
+            }
 		}
 
-		public int SubTotal => Product.Price * Qty;
+		public int SubTotal
+        {
+            get { return Product.Price * Qty; }
+        }
 	}
 }

53968531f858bffdf3fec2048b710398d1c9dee3
zxc5356061 <zxc5356061@gmail.com>
2022-01-27 11:53:51 +0800

    [v] add /Models/Entities/CategoryEntity [v] add /Models/Entities/CustomerEntity [v] add /Models/Entities/OrderProductEntity [v] add /Models/Entities/OrderItemEntity [v] add /Models/Entities/OrderEntity

---
+++ ClothesToU.Site/Models/Entities/Cart/CustomerEntity.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ClothesToU.Site.Models.Entities
+{
+    public class CustomerEntity
+    {
+        public int Id { get; set; }
+        public string CustomerAccount { get; set; }
+    }
+}
